bc567eaa70388fd41be2d6a5d465570573a7fc78
psat6 <psat6@penguin>
2021-02-23 12:49:26 +0100

    commit inicial

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'extension lista tareas',
+    'author': "Monica",
+    'version': '0.1',
+    # any module necessary for this one to work correctly
+    'depends': ['moduloprueba'],
+
+    # always loaded
+    #'data': [
+        #'security/ir.model.access.csv',
+        #'views/views.xml',
+        #'views/menu.xml',
+}
